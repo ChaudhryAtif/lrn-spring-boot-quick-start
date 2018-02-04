074cfc35cf21174998891473262a9e8dd26c8267
Atif Chaudhry <achaudh5@slu.edu>
2018-02-04 14:05:59 -0600

    - Perform crud functionality using repository - Remove hard-coded topics array

--- course-api/src/main/java/com/aic/courseapi/service/TopicService.java
+++ course-api/src/main/java/com/aic/courseapi/service/TopicService.java
@@ -1,44 +1,35 @@
 package com.aic.courseapi.service;
 
 import com.aic.courseapi.domain.Topic;
+import com.aic.courseapi.repository.TopicRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 @Service
 public class TopicService {
 
-    private List<Topic> topics = new ArrayList<>(Arrays.asList(
-            new Topic(1L, "Spring Framework", "Spring Framework Description"),
-            new Topic(2L, "Core Java", "Core Java Description"),
-            new Topic(3L, "JavaScript", "JavaScript Description")
-    ));
+    @Autowired
+    private TopicRepository topicRepository;
 
     public List<Topic> getAll() {
-        return topics;
+        return topicRepository.findAll();
     }
 
     public Topic getOne(Long id) {
-        return topics.stream().filter(topic -> topic.getId().equals(id)).findFirst().get();
+        return topicRepository.findOne(id);
     }
 
     public void add(Topic topic) {
-        topics.add(topic);
+        topicRepository.save(topic);
     }
 
     public void update(Long id, Topic topic) {
-        for (int i = 0; i < topics.size(); i++) {
-            Topic t = topics.get(i);
-            if (t.getId().equals(id)) {
-                topics.set(i, topic);
-                return;
-            }
-        }
+        topicRepository.save(topic);
     }
 
     public void delete(Long id) {
-        topics.removeIf(topic -> topic.getId().equals(id));
+        topicRepository.delete(id);
     }
 }
